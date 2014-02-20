927d1cf573f78f1025001057a34500636e30da16
Aniket Handa <atneik@gmail.com>
2014-02-20 10:33:22 -0800

    location

--- src/com/atneik/ttt/MainActivity.java
+++ src/com/atneik/ttt/MainActivity.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -28,7 +29,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+
+public class MainActivity extends FragmentActivity 
+implements
+ConnectionCallbacks,
+OnConnectionFailedListener,
+LocationListener,
+OnMyLocationButtonClickListener{
 
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -44,6 +60,36 @@ public class MainActivity extends FragmentActivity {
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
+	
+	//Maps
+	//private GoogleMap mMap;
+    private LocationClient mLocationClient;
+    //private TextView mMessageView;
+ // These settings are the same as the settings for the map. They will in fact give you updates
+    // at the maximal rates currently possible.
+    private static final LocationRequest REQUEST = LocationRequest.create()
+            .setInterval(5000)         // 5 seconds
+            .setFastestInterval(16)    // 16ms = 60fps
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	//
+    private static String tweetText = null;
+	
+	private static String disaster = null;
+	private static String location = null;
+	private static String category = null;
+	private static String moreInfo = null;
+	
+	private static void updateTweet(){
+		tweetText = "";
+		if(disaster != null)
+			tweetText += "#" + disaster;
+		if(location != null)
+			tweetText += "#" + location;
+		if(category != null)
+			tweetText += "#" + category;
+		if(moreInfo != null)
+			tweetText += "#" + moreInfo;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -58,16 +104,118 @@ public class MainActivity extends FragmentActivity {
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
+		
+		//mMessageView = (TextView) findViewById(R.id.message_text);
 
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	 @Override
+	    protected void onResume() {
+	        super.onResume();
+	        setUpMapIfNeeded();
+	        setUpLocationClientIfNeeded();
+	        mLocationClient.connect();
+	        
+	    }
 
+	    @Override
+	    public void onPause() {
+	        super.onPause();
+	        if (mLocationClient != null) {
+	            mLocationClient.disconnect();
+	        }
+	    }
+	    
+	    private void setUpMapIfNeeded() {
+	        /*// Do a null check to confirm that we have not already instantiated the map.
+	        if (mMap == null) {
+	            // Try to obtain the map from the SupportMapFragment.
+	            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+	                    .getMap();
+	            // Check if we were successful in obtaining the map.
+	            if (mMap != null) {
+	                mMap.setMyLocationEnabled(true);
+	                mMap.setOnMyLocationButtonClickListener(this);
+	            }
+	        }*/
+	    }
+
+	    private void setUpLocationClientIfNeeded() {
+	        if (mLocationClient == null) {
+	            mLocationClient = new LocationClient(
+	                    getApplicationContext(),
+	                    this,  // ConnectionCallbacks
+	                    this); // OnConnectionFailedListener
+	        }
+	    }
+
+	
+
+	 /**
+     * Button to get current Location. This demonstrates how to get the current Location as required
+     * without needing to register a LocationListener.
+     */
+    public void showMyLocation(View view) {
+        if (mLocationClient != null && mLocationClient.isConnected()) {
+            String msg = "Location = " + mLocationClient.getLastLocation();
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Implementation of {@link LocationListener}.
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        //mMessageView.setText("Location = " + location);
+    	String str = Double.toString(location.getLatitude()) + ", "+ Double.toString(location.getLongitude());
+    	Log.d("lat,long:", str);
+    	
+    	MainActivity.location = location.toString();
+    	
+    }
+    
+    /**
+     * Callback called when connected to GCore. Implementation of {@link ConnectionCallbacks}.
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        mLocationClient.requestLocationUpdates(
+                REQUEST,
+                this);  // LocationListener
+    }
+
+    /**
+     * Callback called when disconnected from GCore. Implementation of {@link ConnectionCallbacks}.
+     */
+    @Override
+    public void onDisconnected() {
+        // Do nothing
+    }
+
+    /**
+     * Implementation of {@link OnConnectionFailedListener}.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // Do nothing
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+	
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
@@ -124,14 +272,14 @@ public class MainActivity extends FragmentActivity {
 		 */
 		public static final String ARG_SECTION_NUMBER = "section_number";
 		
-		private String tweetText = null;
 		
-		private String disaster = null;
-		private String location = null;
-		private String category = null;
-		private String moreInfo = null;
 		
 		public SectionFragment() {
+		}
+		
+		public void setLocation(String loc){
+			location = loc;
+			updateTweet();
 		}
 		
 		@Override
@@ -154,7 +302,7 @@ public class MainActivity extends FragmentActivity {
 				tweetCount.setText(Integer.toString(countLeft));
 			}
 			
-			
+			updateTweet();
 		    
 		    switch(getArguments().getInt(ARG_SECTION_NUMBER)){
 		    	
@@ -191,17 +339,7 @@ public class MainActivity extends FragmentActivity {
 			return rootView;
 		}
 		
-		private void updateTweet(){
-			tweetText = "";
-			if(disaster != null)
-				tweetText += "#" + disaster;
-			if(location != null)
-				tweetText += "#" + location;
-			if(category != null)
-				tweetText += "#" + category;
-			if(moreInfo != null)
-				tweetText += "#" + moreInfo;
-		}
+		
 			
 		Handler mTweetHandler = new Handler();
 	    Runnable mTweetRunnable = new Runnable() {
@@ -280,5 +418,6 @@ public class MainActivity extends FragmentActivity {
 	            R.drawable.sample_6, R.drawable.sample_7
 	    };
 	}
+	
 
 }
